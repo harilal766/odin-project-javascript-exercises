01b8c557f62368ff6d85e4f7920c394eabbe8a38
harilal766 <harilalsunil2@gmai.com>
2025-02-02 10:26:58 +0530

    finished exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string,limit) {
+    resultString = "";
 
+    if (limit < 0) {
+        resultString = "ERROR";
+    }
+    else {
+        for (let i=0; i<limit; i++) {
+            resultString += string;
+        }
+    }
+    return resultString;
+    
 };
 
 // Do not edit below this line
