9486e0b0ba6a711d7cc41bc4a40cbff21488609a
harilal766 <harilalsunil2@gmai.com>
2025-02-02 21:17:50 +0530

    ex 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
 
+    if (!Number.isInteger(min) || !Number.isInteger(max) ||
+        min <0 || max<0)
+    {
+        return "ERROR";
+    }
+    else {
+        if (min > max){
+            const TEMP = min;
+            min = max;
+            max = TEMP;
+        }
+
+        let sum = 0;
+        for(let i=min; i<=max; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
+    
 };
 
+
+console.log(sumAll(1,4))
 // Do not edit below this line
 module.exports = sumAll;
