8c70f18405c6260c750486e25e59d8bc643c88fc
harilal766 <harilasunil2@gmail.com>
2025-02-08 14:35:42 +0530

    added DOM folder for MDN lecture

--- DOM/script.js
+++ DOM/script.js
@@ -58,3 +58,15 @@ btn.addEventListener("click",alertFunction);
 btn.addEventListener("click",function(e){
     e.target.style.background = "blue";
 });
+
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
