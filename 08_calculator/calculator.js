cbffab4ac8d505c63ffe0ce6ad514b65d2e9585c
harilal766 <harilasunil2@gmail.com>
2025-02-20 16:32:42 +0530

    calculator : added codes for power

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,26 @@
-const add = function() {
-	
+const add = function(n1,n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2) {
+	return n1-n2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers = []) {
+	return numbers.reduce((acc,num) => acc+num,0);
 };
 
-const multiply = function() {
-
+const multiply = function(n1,n2) {
+  return n1*n2;
 };
 
-const power = function() {
-	
+const power = function(num,powerNum) {
+	let pow = 1;
+  for (let i=0; i<powerNum; i++){
+    pow *= num;
+  }
+  return pow;
+
 };
 
 const factorial = function() {
