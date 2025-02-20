69f87d0e65e9a8340bc473de74e289e36f7dd373
harilal766 <harilalsunil2@gmai.com>
2025-02-20 18:30:53 +0530

    palindrome : completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    // remove spaces and special characters
+    inputString = inputString.replace(/[^a-zA-Z0-9]/g,"");
+    // lower the text
+    inputString = inputString.toLowerCase();
+    
+    return inputString === inputString.split("").reverse().join("");
 };
 
 // Do not edit below this line
