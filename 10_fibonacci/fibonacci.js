32ffe9037175f3bc396ca083bf8f48aa91c80718
harilal766 <harilalsunil2@gmai.com>
2025-02-20 19:53:03 +0530

    finished fibanocci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    fib = "OOPS"; let fibArray = [0,1];
+    // if input string is not a number convert it to integer
+    if (typeof num !== "number"){
+        num = parseInt(num);
+    }
 
+    // make sure negatives arent accepted
+    if (num <0) return "OOPS";
+    if (num === 0) return num;
+    
+    let firstPrev = 1;
+    let secodPrev= 0;
+
+    for (let i=2; i<= num; i++){
+        let current =  firstPrev + secodPrev;
+        secodPrev = firstPrev;
+        firstPrev = current;
+    }
+    
+    return firstPrev;
+    // 0 => 0
 };
 
 // Do not edit below this line
